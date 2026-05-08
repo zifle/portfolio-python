7566bfba3ba0572dff989ac0273d2aacf18f1af9
Niels Pedersen <zifle@hotmail.com>
2026-05-08 08:48:33 +0200

    use .env file for environment setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
+env = os.environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8xkn!6h8o(9%qc8lumw5zr)wcbljsqw7f=+spgk$cgk77&q^52"
+SECRET_KEY = env.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.get('DEBUG', False)
 
 ALLOWED_HOSTS = [
-    'portfolio.test'
+    env.get('ALLOWED_HOSTS')
 ]
+
 
 
 # Application definition

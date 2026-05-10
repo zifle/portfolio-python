7e9c216a668e1fb568caa141bc6993924ea021d9
Niels Pedersen <zifle@hotmail.com>
2026-05-10 14:28:20 +0200

    add image drop upload

--- frontend/src/pages/admin/albums/imageUpload.vue
+++ frontend/src/pages/admin/albums/imageUpload.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {onMounted, ref, useTemplateRef} from "vue";
+import {onMounted, onUnmounted, ref, useTemplateRef} from "vue";
 import {useAdminAlbumStore} from "../../../store/admin/albums.js";
 
 const albumStore = useAdminAlbumStore();
@@ -9,29 +9,73 @@ const emit = defineEmits(['filesUploaded']);
 const fileUpload = useTemplateRef('fileUpload');
 const uploading = ref(false);
 onMounted(() => {
-    fileUpload.value.addEventListener("change", async (e) => {
-        uploading.value = true;
-        try {
-            const files = e.target.files;
-            const data = new FormData();
-
-            let idx = 0;
-            for (const file of files) {
-                if (!file.type.startsWith("image/")) {
-                    continue;
-                }
-
-                data.append('file_' + idx, file);
-                idx++;
-            }
-
-            const upload_data = await albumStore.uploadImages(data);
-            emit('filesUploaded', upload_data);
-        } finally {
-            uploading.value = false;
-        }
+    fileUpload.value.addEventListener("change", (e) => {
+        const files = e.target.files;
+        uploadImages(files);
     });
 });
+
+async function uploadImages(files) {
+    uploading.value = true;
+
+    try {
+        const data = new FormData();
+
+        let idx = 0;
+        for (const file of files) {
+            if (!file.type.startsWith("image/")) {
+                continue;
+            }
+
+            data.append('file_' + idx, file);
+            idx++;
+        }
+
+        if (idx > 0) {
+            const upload_data = await albumStore.uploadImages(data);
+            emit('filesUploaded', upload_data);
+        } else {
+            console.warn('Empty file list!', files);
+        }
+    } finally {
+        uploading.value = false;
+    }
+}
+
+const dropZone = useTemplateRef('fileDropZone');
+function dragoverFile(e) {
+    const fileItems = [...e.dataTransfer.items].filter( i => i.kind === 'file');
+    if (fileItems.length > 0) {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'copy';
+        dropZone.value.classList.remove('d-none');
+    } else {
+        dropZone.value.classList.add('d-none');
+    }
+}
+function dropFiles(e) {
+    const fileItems = [...e.dataTransfer.items].filter( i => i.kind === 'file');
+    if (fileItems.length > 0) {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'copy';
+        uploadImages(fileItems.map(i => i.getAsFile()));
+        dragEnd();
+    }
+}
+function dragEnd() {
+    dropZone.value.classList.add('d-none');
+}
+onMounted(() => {
+    document.body.addEventListener('dragover', dragoverFile);
+    document.body.addEventListener('dragend', dragEnd);
+    document.body.addEventListener('dragleave', dragEnd);
+    dropZone.value.addEventListener('drop', dropFiles);
+});
+onUnmounted(() => {
+    document.body.removeEventListener('dragover', dragoverFile);
+    document.body.removeEventListener('dragend', dragEnd);
+    document.body.removeEventListener('dragleave', dragEnd);
+})
 </script>
 
 <template>
@@ -43,8 +87,22 @@ onMounted(() => {
     <div v-if="uploading" class="uploading position-fixed">
         Uploading images ...
     </div>
+
+    <div class="file-dropzone d-none" ref="fileDropZone">+</div>
 </template>
 
 <style scoped>
-
+.file-dropzone {
+    position: fixed;
+    display: flex;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    align-items: center;
+    justify-content: center;
+    font-size: 20rem;
+    user-select: none;
+}
 </style>
